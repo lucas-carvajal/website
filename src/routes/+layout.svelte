5b945f1e41250cc7f95c9c1ff19b20609450afcd
lucas <lcs.carvajal@gmail.com>
2023-01-25 22:07:58 +0100

    set up side navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,46 @@
+
+<div class="nav">
+    <div class="elements">
+        <a href="/">Lucas</a>
+        <a href="/work">Work</a>
+        <a href="/education">Education</a>
+        <a href="/projects">Projects</a>
+        <a href="/contact">Contact</a>
+    </div>
+</div>
+ 
+<div class="main">
+    <slot></slot>
+</div>
+
+<style>
+    .nav {
+        position: fixed;
+        width: 20%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        z-index: 1;
+        overflow-x: hidden;
+        padding-top: auto;
+        padding-bottom: auto;
+        padding-left: 20px;
+        display: flex;
+        justify-content: center;
+        align-items: center; 
+    }
+
+    a {
+        display: block;
+        margin: 0.8em 0;
+        padding: 10px;
+        text-decoration: none;
+        color: black;
+        font-size: 3vw;
+    }
+
+    .main {
+        margin-left: 20%;
+        padding: 4em;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Hey! I'm Lucas</p> 
+<p>Visit my LinkedIn <a href="https://www.linkedin.com/in/lucas-carvajal/">here</a></p>
+
+<a href="/about">About my site</a>

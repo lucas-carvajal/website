b1aa4f3a18f3fd050394b995df97dd95975fe5ca
lucas <lcs.carvajal@gmail.com>
2023-01-27 20:50:54 +0100

    make animation work

--- src/lib/NavButton.svelte
+++ src/lib/NavButton.svelte
@@ -7,42 +7,67 @@
 	let gradientState = `white, white`
 	let interval: any
 	
-    const createGradient = (percentage: number) => 
-        `to right, black 0%, black ${percentage}%, white ${percentage}%, white 100%`
-
-    let gr1 = `to right, black 0%, black 47%, white 47%, white 100%`;
-    let colorr = 'orange';
+    const createGradient = (colorIn: string, colorOut: string, percentage: number) => 
+        `to right, ${colorIn} 0%, ${colorIn} ${percentage}%, ${colorOut} ${percentage}%, ${colorOut} 100%`
 
 	const animation = (direction: number) => {
 			clearInterval(interval)
 			interval = setInterval(() => {
 				tick <= 100 ? 
-					gradientState = createGradient(tick) :
+					gradientState = createGradient("black", "white", tick) :
 			  	    gradientState = `black, black`;
 				direction === 1 ? 
                     tick = tick + 2 :
                     tick = tick - 2;
                 console.log(gradientState)
-			}, 10)
+			}, 0.0001)
 	}
+
+    // const animateIn = () => {
+    //         clearInterval(interval)
+    //         interval = setInterval(() => {
+    //             tick <= 100 ? 
+    //                 gradientState = createGradient("black", "white", tick) :
+    //                 gradientState = `black, black`;
+    //             tick = tick + 2
+    //             console.log(title)
+    //             console.log(gradientState)
+    //         }, 0.0001)
+    // }
+
+    // const animateOut = () => {
+    //         clearInterval(interval)
+    //         interval = setInterval(() => {
+    //             tick <= 100 ? 
+    //                 gradientState = createGradient("white", "black", tick) :
+    //                 gradientState = `white, white`;
+    //             tick = tick - 2
+    //             console.log(title)
+    //             console.log(gradientState)
+    //         }, 0.0001)
+    // }
 	
 	$: if(tick >= 101) tick = 100
 	$: if(tick < 0) tick = 0
 </script>
 
+
+<!-- <div on:mouseenter={() => animateIn()}
+    on:mouseleave={() => animateOut()}
+> -->
 <div on:mouseenter={() => animation(1)}
     on:mouseleave={() => animation(0)}
 >
-    <div class="element">
-        <a href={route} style:background={`linear-gradient(${gradientState}) left bottom white no-repeat`}>
-        <!-- <a href={route} 
-            style:border-bottom={`0.1em solid linear-gradient(to right, black 0%, black 47%, white 47%, white 100%)`}    
-            style:border-color={`linear-gradient(orange, red)`}
-            style:border-image-slice={`1`}
-        > -->
-            {title}
-        </a>
-    </div>
+<div class="element">
+    <a href={route} 
+        style:border-style={`solid`}
+        style:border-width={`0.1em`}
+        style:border-image={`linear-gradient(${gradientState}) 0% 0 100% 0/0px 0 3px 0 stretch`}
+    >
+        {title}
+    </a>
+</div>
+
 </div>
 
 <style>
@@ -58,9 +83,9 @@
         text-decoration: none;
         color: black;
         font-size: 2vw;
-        padding-bottom: 0.1em;
+        /* padding-bottom: 0.1em; */
         /* background: linear-gradient(to right, black 0%, black 47%, white 47%, white 100%) bottom white no-repeat; */
-        background-size: 100% 0.1em;
+        /* background-size: 100% 0.1em; */
         /* letter-spacing: 1px; */
     }
 

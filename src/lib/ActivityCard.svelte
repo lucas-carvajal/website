c21a13421a7cfc9c1893c7a0e77b142e58f4653a
lucas <lcs.carvajal@gmail.com>
2023-02-04 22:59:45 +0100

    add activity card component

---
+++ src/lib/ActivityCard.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    export let institution: string;
+    export let location: string;
+    export let title: string;
+    export let dates: string;
+    export let description: string;
+</script>
+
+<div class="card">
+    <div class="header">
+        <h2 class="institution">{institution}</h2>
+        <p class="location">{location}</p>
+    </div>
+    <h4 class="title">{title}</h4>
+    <p class="dates">{dates}</p>
+    <p class="description">{description}</p>
+</div>
+
+<style>
+    .card {
+        margin: 3em;
+    }
+    .header {
+        display: inline;
+    }
+</style>
--- src/routes/education/+page.svelte
+++ src/routes/education/+page.svelte
@@ -1,1 +1,13 @@
+<script lang="ts">
+    import ActivityCard from "../../lib/ActivityCard.svelte";
+</script>
 <h1>Education</h1>
+
+<ActivityCard 
+    institution="San Francisco State University"
+    location="San Francisco, CA" 
+    title="Semester Abroad" 
+    dates="August 2021 - December 2021"
+    description="Selected Coursework:\n 
+        - Introduction to Computer Science"
+    />
